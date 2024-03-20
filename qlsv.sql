16c77ba9bb5332d536a5a21ec84cceabbbfa5857
hungshadow27 <you@example.com>
2024-03-20 22:46:39 +0700

    update many

--- qlsv.sql
+++ qlsv.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Mar 19, 2024 at 01:47 PM
+-- Generation Time: Mar 20, 2024 at 04:46 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -70,10 +70,22 @@ INSERT INTO `department` (`department_id`, `name`) VALUES
 CREATE TABLE `score` (
   `score_id` int(11) NOT NULL,
   `value` int(11) NOT NULL,
-  `student_id` int(11) NOT NULL,
-  `subject_id` int(11) NOT NULL,
+  `student_id` varchar(50) NOT NULL,
+  `subject_id` varchar(50) NOT NULL,
   `date` datetime NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `score`
+--
+
+INSERT INTO `score` (`score_id`, `value`, `student_id`, `subject_id`, `date`) VALUES
+(1, 9, '100000', 'LTPHP', '2024-03-20 17:14:57'),
+(2, 5, '100004', 'LTPHP', '2024-03-20 22:12:14'),
+(3, 7, '100003', 'LTPHP', '2024-03-20 22:12:14'),
+(4, 3, '100003', 'LTHDT', '2024-03-20 22:12:50'),
+(5, 3, '100004', 'LTHDT', '2024-03-20 22:12:50'),
+(6, 5, '100000', 'LTHDT', '2024-03-20 22:12:50');
 
 -- --------------------------------------------------------
 
@@ -108,17 +120,40 @@ INSERT INTO `student` (`student_id`, `name`, `gender`, `date_of_birth`, `address
 CREATE TABLE `subject` (
   `subject_id` varchar(50) NOT NULL,
   `name` varchar(255) NOT NULL,
-  `department_id` varchar(50) NOT NULL
+  `department_id` varchar(50) NOT NULL,
+  `status` int(1) NOT NULL DEFAULT 0,
+  `registered_students` varchar(255) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 
 --
 -- Dumping data for table `subject`
 --
 
-INSERT INTO `subject` (`subject_id`, `name`, `department_id`) VALUES
-('LSD', 'Lịch sử Đảng', 'LLCT'),
-('LTHDT', 'Lập trình hướng đội tượng', 'CNTT'),
-('LTPHP', 'Lập trình PHP', 'CNTT');
+INSERT INTO `subject` (`subject_id`, `name`, `department_id`, `status`, `registered_students`) VALUES
+('LSD', 'Lịch sử Đảng', 'LLCT', 1, 'a:2:{i:0;i:100000;i:1;i:100003;}'),
+('LTHDT', 'Lập trình hướng đội tượng', 'CNTT', 1, 'a:3:{i:0;i:100003;i:1;s:6:\"100004\";i:2;i:100000;}'),
+('LTPHP', 'Lập trình PHP', 'CNTT', 1, 'a:3:{i:0;i:100000;i:1;i:100004;i:2;i:100003;}');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+CREATE TABLE `user` (
+  `user_id` int(11) NOT NULL,
+  `username` varchar(50) NOT NULL,
+  `password` varchar(50) NOT NULL,
+  `role` varchar(50) NOT NULL DEFAULT 'student',
+  `created_at` datetime NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `user`
+--
+
+INSERT INTO `user` (`user_id`, `username`, `password`, `role`, `created_at`) VALUES
+(1, 'admin', '123456', 'admin', '2024-03-20 22:20:20');
 
 --
 -- Indexes for dumped tables
@@ -137,6 +172,12 @@ ALTER TABLE `department`
   ADD PRIMARY KEY (`department_id`);
 
 --
+-- Indexes for table `score`
+--
+ALTER TABLE `score`
+  ADD PRIMARY KEY (`score_id`);
+
+--
 -- Indexes for table `student`
 --
 ALTER TABLE `student`
@@ -149,14 +190,32 @@ ALTER TABLE `subject`
   ADD PRIMARY KEY (`subject_id`);
 
 --
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`user_id`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
+
+--
+-- AUTO_INCREMENT for table `score`
+--
+ALTER TABLE `score`
+  MODIFY `score_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=7;
 
 --
 -- AUTO_INCREMENT for table `student`
 --
 ALTER TABLE `student`
   MODIFY `student_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=100005;
+
+--
+-- AUTO_INCREMENT for table `user`
+--
+ALTER TABLE `user`
+  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
